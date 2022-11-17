ec4ceec3b2fcc8f2481aa6f1ab9ffd448fc057af
S <4phr0d1t3.2ms4u@gmail.com>
2022-11-17 02:05:04 -0600

    p4.c v0.8

--- p4.c
+++ p4.c
@@ -9,30 +9,24 @@ void longCad() {
 }
 
 void solve(char state, char deltaPot) {
-	
 	if (str[deltaPot]) {
 		printf(" -%d-> q%d \n δ(q%d, %c)", deltaPot, state, state, str[deltaPot]);
 
-		switch (state) {
-			case 0:
-				if (str[deltaPot] == 'b')
-					solve(0, deltaPot+1); // δ(q0, b) -> q0
-				else if(str[deltaPot] == 'a') {
-					solve(0, deltaPot+1); // δ(q0, a) -> q0
-					solve(1, deltaPot+1); // δ(q0, a) -> q1
-				}
-			break;
-
-			case 1:
-				if (str[deltaPot] == 'b')
-					solve(2, deltaPot+1); // δ(q1, b) -> q2
-			break;
-
-			default:
-				printf(" -%d-> Ø" , deltaPot+1); // -> Ø
-			break;
-		}
+		if(state == 0)
+			if (str[deltaPot] == 'b')
+				solve(0, deltaPot+1); // δ(q0, b) -> q0
+			else if(str[deltaPot] == 'a') {
+				solve(0, deltaPot+1); // δ(q0, a) -> q0
+				solve(1, deltaPot+1); // δ(q0, a) -> q1
+			}
+		else if(state == 1)
+			if (str[deltaPot] == 'b')
+				solve(2, deltaPot+1); // δ(q1, b) -> q2
+		else if(state == 2)
+			printf(" -%d-> Ø" , deltaPot+1); // -> Ø
 	}
+	// else if(state == 0 || state == 2)
+	// 	printf("\nEs valida\n\n");
 }
 
 int main(int argc, char const *argv[]) {
