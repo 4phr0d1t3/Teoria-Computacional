8c0f17257c45460fbce1974ee3c4e91e333646f6
Sebastian Ruiz Uvalle <64220877+4phr0d1t3@users.noreply.github.com>
2022-11-17 13:18:33 -0600

    p4.c v2.1

--- p4.c
+++ p4.c
@@ -6,43 +6,29 @@ char size, num = 0;
 
 void solve(char state, char deltaPot) {
 	if(str[deltaPot]) {
-		//printf("\tδ(q%d, %c) -%d-> ", state, str[deltaPot], deltaPot);
-		if(state == 0)
+		if(state == 0) {
 			if(str[deltaPot] == 'b') {
-				//printf("q0\n");
 				path[deltaPot] = 0;
 				solve(0, deltaPot+1); // δ(q0, b) -> q0
 			}
 			else if(str[deltaPot] == 'a') {
-				//printf("q0\n");
 				path[deltaPot] = 0;
 				solve(0, deltaPot+1); // δ(q0, a) -> q0
 				
-				//printf(" Nuevo camino en: -%d-> q1\n", deltaPot);
 				path[deltaPot] = 1;
 				solve(1, deltaPot+1); // δ(q0, a) -> q1
 			}
-			else{}
-				//printf("Ø\n       --- No valido ---\n"); // -> Ø
+		}
 		else if(state == 1)
 			if(str[deltaPot] == 'b') {
-				//printf("q2\n");
 				path[deltaPot] = 2;
 				solve(2, deltaPot+1); // δ(q1, b) -> q2
 			}
-			else{}
-				//printf("Ø\n       --- No valido ---\n"); // -> Ø
-		else if(state == 2){}
-			//printf("Ø\n       --- No valido ---\n"); // -> Ø
 	}
-	//else if(deltaPot != size)
-	//	printf(" -%d-> q%d\n", deltaPot, state);
-	//else if(state != 0 && state !=2)
-	//	printf("       --- No valido ---\n");
 	else if(deltaPot == size && (state == 0 || state == 2)){
-		printf("Camino Valido:\t-> q0");
+		printf("Camino Valido:\n\t--> q0");
 		for(char i = 0; i<size; ++i)
-			printf(" -> q%d", path[i]);
+			printf(" -%c-> q%d", str[i], path[i]);
 		printf("\n");
 		++num;
 	}
