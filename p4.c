aef908817a38efeff54b32b530c1873cb87e6d57
S <4phr0d1t3.2ms4u@gmail.com>
2022-11-17 01:57:42 -0600

    p4.c v0.7

--- p4.c
+++ p4.c
@@ -2,35 +2,37 @@
 #include <stdlib.h>
 
 char str[10];
+char size;
+
+void longCad() {
+	for(size = 0; str[size]; ++size);
+}
 
 void solve(char state, char deltaPot) {
-	if(str[deltaPot]) {
+	
+	if (str[deltaPot]) {
 		printf(" -%d-> q%d \n δ(q%d, %c)", deltaPot, state, state, str[deltaPot]);
 
-		switch (str[deltaPot]) {
-			case 'a':
-				if(state == 0) {
+		switch (state) {
+			case 0:
+				if (str[deltaPot] == 'b')
+					solve(0, deltaPot+1); // δ(q0, b) -> q0
+				else if(str[deltaPot] == 'a') {
 					solve(0, deltaPot+1); // δ(q0, a) -> q0
-					printf("\n\tNuevo camino: ");
 					solve(1, deltaPot+1); // δ(q0, a) -> q1
 				}
-				else if (state == 1)
-					solve(2, deltaPot+1); // δ(q1, a) -> q2
 			break;
 
-			case 'b':
-				if(state == 0)
-					solve(0, deltaPot+1); // δ(q0, b) -> q0
+			case 1:
+				if (str[deltaPot] == 'b')
+					solve(2, deltaPot+1); // δ(q1, b) -> q2
 			break;
 
 			default:
-				printf(" -> Ø\n----- No es valida -----\n"); // -> Ø
+				printf(" -%d-> Ø" , deltaPot+1); // -> Ø
+			break;
 		}
 	}
-	else if(state == 0 || state == 2) // q0 || q2
-		printf(" -%d-> q%d\n\nLa cadena es valida\n", deltaPot, state);
-	else
-		printf(" La cadena no es valida");
 }
 
 int main(int argc, char const *argv[]) {
@@ -38,6 +40,9 @@ int main(int argc, char const *argv[]) {
 	fflush(stdin);
 	scanf("%s", str);
 
+	longCad();
+
+	printf("\n");
 	solve(0, 0);
 	printf("\n");
 
