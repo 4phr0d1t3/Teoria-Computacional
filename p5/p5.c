4b75e97efdafca46da75a3758a3ec81d45930066
S <4phr0d1t3.2ms4u@gmail.com>
2022-11-22 12:14:44 -0600

    p5.c v1

--- p5/p5.c
+++ p5/p5.c
@@ -4,7 +4,7 @@
 char str[100], *path;
 char size, x, num = 0;
 
-char *states, *sigmaM, s, *fS, (*deltaTab)[4];
+char *states, *sigmaM, s, *fS, *deltaTab;
 
 char* getFromFile() {
 	FILE *textFile;
@@ -12,6 +12,8 @@ char* getFromFile() {
 	long numbytes;
 
 	textFile = fopen("AFND.txt", "r");
+	if(textFile == NULL)
+		return 0;
 
 	fseek(textFile, 0L, SEEK_END);
 	numbytes = ftell(textFile);
@@ -22,11 +24,6 @@ char* getFromFile() {
 	fread(text, sizeof(char), numbytes, textFile);
 	fclose(textFile);
 
-	// Just for printing
-	// for (char i = 0; text[i]; ++i)
-	// 	// if(text[i] != 10 && text[i] != 13)
-	// 	if(text[i] != 10)
-	// 		printf("%d = %c\n", i, text[i]);
 	return text;
 }
 
@@ -35,15 +32,19 @@ void printAFN() {
 	for(char i = 0; states[i]; ++i) printf(" %c", states[i]);
 	
 	printf("\nAlfabeto =");
-	for(char i = 0;sigmaM[i]; ++i) printf(" %c",sigmaM[i]);
+	for(char i = 0; sigmaM[i]; ++i) printf(" %c",sigmaM[i]);
 
 	printf("\nEstado inicial = %c", s);
 
 	printf("\nEstado final =");
-	for(char i = 0;fS[i]; ++i) printf(" %c",fS[i]);
+	for(char i = 0; fS[i]; ++i) printf(" %c",fS[i]);
 
-	printf("\nDelta:");
-	for(char n = 0; n<x; ++n) printf("\t%s\n", deltaTab[n]);
+	printf("\nDelta:\n");
+	for(char i = 0; deltaTab[i]; ++i)
+		if((i+1)%3 == 0)
+			printf(" %c\n", deltaTab[i]);
+		else
+			printf(" %c", deltaTab[i]);
 }
 
 char* getInfo(char *text, char *i) {
@@ -60,10 +61,13 @@ char* getInfo(char *text, char *i) {
 	return obj;
 }
 
-void getAFN() {
+char getAFN() {
 	char *text = getFromFile();
+	if(!text)
+		return 1;
+
 	char auxSize, aux, i = 0;
-	
+
 	states = getInfo(text, &i); // states
 	++i;
 	sigmaM = getInfo(text, &i); // alphabet
@@ -73,27 +77,19 @@ void getAFN() {
 
 	// delta table
 	for (aux = ++i, x = 1; text[aux]; ++aux) if(text[aux] == 10) ++x;
-	char auxDeltaTab[x][4];
-	for(char n = 0, m = 0, aux = i; text[aux]; ++aux)
-		if(text[aux] != 10 && text[aux] != 13 && text[aux] != ',') {
-			auxDeltaTab[n][m] = text[aux];
-			++m;
-		}
-		else if(text[aux] == 13) {
-			auxDeltaTab[n][3] = '\0';
-			++aux;
-			++n;
-			m = 0;
-		}
-	deltaTab = auxDeltaTab;
-	printAFN();
-}
+	deltaTab = (char *) malloc((x*3+1)*sizeof(char));
 
-char existsState(char state) {
-	for(char i = 0; i < x; ++i)
-		if(state == deltaTab[i][0])
-			return i;
-	return -1;
+	char j;
+	for(j = 0, aux = i; text[aux]; ++aux)
+		if(text[aux] != 10 && text[aux] != 13 && text[aux] != ',') {
+			deltaTab[j] = text[aux];
+			++j;
+		}
+	deltaTab[j] = '\0';
+
+	printAFN();
+
+	return 0;
 }
 
 char isFinalState(char state) {
@@ -104,23 +100,12 @@ char isFinalState(char state) {
 }
 
 void solve(char state, char deltaPot) {
-	printf("\n----%c----\n", str[deltaPot]);
-	for(char n = 0; n<x; ++n) printf("\t%s\n", deltaTab[n]);
-	printf("Buscando: %c, %c\n", state, str[deltaPot]);
-
 	if(str[deltaPot]) {
-		for(char aux = 0; aux < x; ++aux) {
-			printf("Probando: %c %c %c\n", deltaTab[aux][0], deltaTab[aux][1], deltaTab[aux][2]);
-			// for(char n = 0; n<x; ++n) printf("\t%s\n", deltaTab[n]);
-
-			if(state == deltaTab[aux][0] && str[deltaPot] == deltaTab[aux][1]) {
-				printf("Encontrado: %c = %c, %c = %c -> %c\n", state, deltaTab[aux][0], str[deltaPot], deltaTab[aux][1], deltaTab[aux][2]);
-				path[deltaPot] = deltaTab[aux][2];
-				// for(char n = 0; n<x; ++n) printf("\t%s\n", deltaTab[n]);
-
-				solve(deltaTab[aux][2], deltaPot + 1);
+		for(char aux = 0; aux < x; ++aux)
+			if(state == deltaTab[aux*3] && str[deltaPot] ==  deltaTab[aux*3+1]) {
+				path[deltaPot] = deltaTab[aux*3+2];
+				solve(deltaTab[aux*3+2], deltaPot + 1);
 			}
-		}
 	}
 	else if(deltaPot == size && isFinalState(state)) {
 		printf("Camino Valido:\n\t--> q%c", s);
@@ -133,19 +118,22 @@ void solve(char state, char deltaPot) {
 
 int main(int argc, char const *argv[]) {
 	printf("Usando el AFN con:\n");
-	getAFN();
+	char err = getAFN();
+	if(err)
+		printf("\t(Err) No se encontro el archivo\n");
+	else {
+		printf("Ingrese la cadena a verificar: ");
+		fflush(stdin);
+		scanf("%s", str);
 
-	printf("Ingrese la cadena a verificar: ");
-	fflush(stdin);
-	scanf("%s", str);
+		for(size = 0; str[size]; ++size);
+		char auxPath[size];
+		path = auxPath;
 
-	for(size = 0; str[size]; ++size);
-	char auxPath[size];
-	path = auxPath;
+		solve('0', 0);
 
-	solve('0', 0);
+		printf("Caminos validos = %d\n", num);
+	}
 
-	printf("Caminos validos = %d\n", num);
-	
 	return 0;
 }
